1f321deba651165d4b1169f943f014d0c59d5fb9
Ivan <wasserleu@gmail.com>
2023-09-15 21:53:36 +0300

    hw5 done

---
+++ hw5/src/main/java/telephoneBook/Main.java
@@ -0,0 +1,30 @@
+package telephoneBook;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        TreeMap<String, ArrayList<Long>> phoneBookTMap = new TreeMap<>();
+        String telBookFile = "Phone_Book.txt";
+        BufferedReader br = new BufferedReader(new FileReader(telBookFile));
+        String str;
+//        long num = 89_052_345_555l;
+        while ((str = br.readLine()) != null) {
+            String[] strArr = str.split(" ");
+            if (phoneBookTMap.containsKey(strArr[0])) {
+                ArrayList<Long> tempList = phoneBookTMap.get(strArr[0]);
+                tempList.add(Long.valueOf(strArr[1]));
+                phoneBookTMap.put(strArr[0], tempList);
+            } else phoneBookTMap.put(strArr[0], new ArrayList<>(Arrays.asList(Long.parseLong(strArr[1]))));
+        }
+        br.close();
+        System.out.println("phoneBookTMap = " + phoneBookTMap);
+        phoneBookTMap.entrySet()
+                .stream()
+                .sorted(Comparator.comparingInt(el -> -el.getValue().size()))
+                .forEach(System.out::println);
+    }
+}
